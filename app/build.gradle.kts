07c6e44b356ff1af175f23b427ea2c018f364a55
SamH95 <haubrich.sam@outlook.com>
2026-05-11 14:11:17 +0200

    prepare patch version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.samwise.unshelved"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "1.4.6"
+        versionCode = 23
+        versionName = "1.4.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
